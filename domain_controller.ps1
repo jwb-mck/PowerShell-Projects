95c7e3f4fac1fbe0cd04280ee65162ec397a10e8
Joel McKenzie <54679278+jwb-mck@users.noreply.github.com>
2022-08-26 16:40:22 +0100

    Add files via upload

---
+++ domain_controller.ps1
@@ -0,0 +1,5 @@
+﻿Install-WindowsFeature AD-Domain-Services
+Install-ADDSForest -DomainName development.imcktraining.com
+
+#Prompt for a SafeMode Administrator Password will appear.
+#Note to self: Look into adding this as a secure string.
